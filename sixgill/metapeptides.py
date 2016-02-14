269f4f84c50671441a645b26be619e3711e0c68d
dhmay <dhmay@fhcrc.org>
2016-02-14 15:41:20 -0800

    renamed proteinlet to metapeptide

--- sixgill/metapeptides.py
+++ sixgill/metapeptides.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-The central module for building and interacting with proteinlet databases.
+The central module for building and interacting with metapeptide databases.
 
 Proteinlet databases are bgzipped tab-separated value files. This module does not handle
 the compression! As far as this module is concerned it is reading and writing .tsv files.
@@ -25,7 +25,7 @@ logger = logging.getLogger(__name__)
 # the names of the columns in a tide-index read-peptides file
 TIDEINDEX_COLUMNS = ['sequence', 'protein id']
 
-# minimum length of a proteinlet to include
+# minimum length of a metapeptide to include
 DEFAULT_MIN_PROTEINLET_AALENGTH = 10
 
 # this regex will match fully-tryptic peptides
@@ -39,7 +39,7 @@ DEFAULT_MIN_AA_QUALSCORE = 30
 DEFAULT_MIN_ORF_LENGTH = 40
 # default minimum AA length of longest tryptic peptide
 DEFAULT_MIN_LONGEST_PEPTIDE_LENGTH = 7
-# default number of reads to keep a proteinlet
+# default number of reads to keep a metapeptide
 DEFAULT_MIN_READ_COUNT = 2
 
 PROTEINLETDB_COLUMNS = ['sequence', 'length', 'min_qualscore', 'n_reads',
@@ -81,22 +81,22 @@ def load_peptide_protein_map_from_tide_index(read_tide_index_file, peptides_to_k
     return result
 
 
-def extract_read_proteinlets(ntseq, phred_qualscores, min_proteinlet_length, min_basecall_qual,
+def extract_read_metapeptides(ntseq, phred_qualscores, min_metapeptide_length, min_basecall_qual,
                              min_orf_length, min_longest_tryppep_length):
     """
-    Extract all proteinlets from a read that match the filtering criteria. Don't check for duplicates, return
+    Extract all metapeptides from a read that match the filtering criteria. Don't check for duplicates, return
     everything. This method does all the heavy lifting.
 
-    Return value is a list of all the proteinlets and then a tuple of accounting information for use
+    Return value is a list of all the metapeptides and then a tuple of accounting information for use
     in charting.
 
     :param ntseq: read nucleotide sequence
     :param phred_qualscores: quality scores for each base
-    :param min_proteinlet_length: minimum length of a proteinlet to return
-    :param min_basecall_qual: minimum minimum basecall quality of a proteinlet to return
+    :param min_metapeptide_length: minimum length of a metapeptide to return
+    :param min_basecall_qual: minimum minimum basecall quality of a metapeptide to return
     :param min_orf_length: minimum length of the ORF to return
     :param min_longest_tryppep_length:
-    :return: a tuple of [result proteinlets], (n_discarded_tooshort, n_discarded_minqualscore,
+    :return: a tuple of [result metapeptides], (n_discarded_tooshort, n_discarded_minqualscore,
                                 n_discarded_stopcodon, n_discarded_longestpep_tooshort,
                                 n_discarded_toofew_trypticsites))
     """
@@ -120,8 +120,8 @@ def extract_read_proteinlets(ntseq, phred_qualscores, min_proteinlet_length, min
     n_discarded_longestpep_tooshort = 0
     n_discarded_toofew_trypticsites = 0
 
-    # iterate over all the frames and find the proteinlets.
-    result_proteinlets = []
+    # iterate over all the frames and find the metapeptides.
+    result_metapeptides = []
     for frame in xrange(0, 6):
         is_reverse = frame > 2
         nt_shift = frame % 3
@@ -139,7 +139,7 @@ def extract_read_proteinlets(ntseq, phred_qualscores, min_proteinlet_length, min
         aa_seq = dna.forward_translate_dna_oneframe(seq_to_translate, nt_shift, stop_before_stop=False)
 
         # if too short, discard
-        if len(aa_seq) < min_proteinlet_length:
+        if len(aa_seq) < min_metapeptide_length:
             n_discarded_tooshort += 1
             continue
 
@@ -166,20 +166,20 @@ def extract_read_proteinlets(ntseq, phred_qualscores, min_proteinlet_length, min
 
         # move in from the left to the first tryptic end
         # move in from the right to the last tryptic end
-        tryppeps_in_proteinletseq = tryptic_peptides[1:len(tryptic_peptides) - 1]
+        tryppeps_in_metapeptideseq = tryptic_peptides[1:len(tryptic_peptides) - 1]
         aa_startindex = len(tryptic_peptides[0])
 
-        # construct the proteinlet AA sequence
-        proteinlet_seq = ''.join(tryppeps_in_proteinletseq)
-        logger.debug("        proteinlet_seq: %s" % proteinlet_seq)
+        # construct the metapeptide AA sequence
+        metapeptide_seq = ''.join(tryppeps_in_metapeptideseq)
+        logger.debug("        metapeptide_seq: %s" % metapeptide_seq)
 
         # if too short, discard
-        if len(proteinlet_seq) < min_proteinlet_length:
+        if len(metapeptide_seq) < min_metapeptide_length:
             n_discarded_tooshort += 1
             continue
 
         # check to see if any tryptic peptides are long enough
-        max_tryppep_length = max([len(tryppep) for tryppep in tryppeps_in_proteinletseq])
+        max_tryppep_length = max([len(tryppep) for tryppep in tryppeps_in_metapeptideseq])
         if max_tryppep_length < min_longest_tryppep_length:
             # if there are no tryptic peptides long enough, return None
             n_discarded_longestpep_tooshort += 1
@@ -191,117 +191,117 @@ def extract_read_proteinlets(ntseq, phred_qualscores, min_proteinlet_length, min
         if min_basecall_qual > 0:
             if logger.isEnabledFor(logging.DEBUG):
                 logger.debug("        qualscores: %s" %
-                             ','.join([str(x) for x in read_qualscores_list[aa_startindex:aa_startindex + len(proteinlet_seq)]]))
-            aa_minqualscore_list = [0] * len(proteinlet_seq)
+                             ','.join([str(x) for x in read_qualscores_list[aa_startindex:aa_startindex + len(metapeptide_seq)]]))
+            aa_minqualscore_list = [0] * len(metapeptide_seq)
             should_stop_lowqual = False
-            for proteinletseq_idx in xrange(0, len(proteinlet_seq)):
-                aa_idx = aa_startindex + proteinletseq_idx
+            for metapeptideseq_idx in xrange(0, len(metapeptide_seq)):
+                aa_idx = aa_startindex + metapeptideseq_idx
                 first_codonpos = aa_idx * 3 + nt_shift
                 aa_minqual = min(read_qualscores_list[first_codonpos:first_codonpos + 3])
                 if aa_minqual < min_basecall_qual:
                     should_stop_lowqual = True
                     break
-                aa_minqualscore_list[proteinletseq_idx] = aa_minqual
+                aa_minqualscore_list[metapeptideseq_idx] = aa_minqual
             if should_stop_lowqual:
                 logger.debug("        low quality.")
                 n_discarded_minqualscore += 1
                 continue
             min_aaqual = min(aa_minqualscore_list)
 
-        logger.debug("        keeping proteinlet!")
+        logger.debug("        keeping metapeptide!")
 
-        # calculate the nucleotide sequence of this proteinlet
-        proteinlet_ntseq_startpos = 3 * aa_startindex
-        proteinlet_nt_seq = nt_seq_for_full_aaseq[proteinlet_ntseq_startpos:proteinlet_ntseq_startpos + 3 * len(proteinlet_seq)]
+        # calculate the nucleotide sequence of this metapeptide
+        metapeptide_ntseq_startpos = 3 * aa_startindex
+        metapeptide_nt_seq = nt_seq_for_full_aaseq[metapeptide_ntseq_startpos:metapeptide_ntseq_startpos + 3 * len(metapeptide_seq)]
 
         # debug logging
-        logger.debug("** %d  %d" % (aa_startindex, proteinlet_ntseq_startpos))
-        logger.debug("        nt seq = %s" % proteinlet_nt_seq)
+        logger.debug("** %d  %d" % (aa_startindex, metapeptide_ntseq_startpos))
+        logger.debug("        nt seq = %s" % metapeptide_nt_seq)
         if logger.isEnabledFor(logging.DEBUG):
-            assert(len(proteinlet_seq) * 3 == len(proteinlet_nt_seq))
-            if dna.forward_translate_dna_firstframe(proteinlet_nt_seq) != proteinlet_seq:
+            assert(len(metapeptide_seq) * 3 == len(metapeptide_nt_seq))
+            if dna.forward_translate_dna_firstframe(metapeptide_nt_seq) != metapeptide_seq:
                 raise ValueError("forward translation of nt seq:\n %s, does not match aa seq: \n %s" %
-                                 (dna.forward_translate_dna_firstframe(proteinlet_nt_seq),
-                                  proteinlet_seq))
+                                 (dna.forward_translate_dna_firstframe(metapeptide_nt_seq),
+                                  metapeptide_seq))
 
-        # add the proteinlet to the list for this read
-        result_proteinlets.append(Proteinlet(proteinlet_seq, min_aaqual,
-                                             len(nt_seq_for_full_aaseq), [proteinlet_nt_seq], 1))
+        # add the metapeptide to the list for this read
+        result_metapeptides.append(Proteinlet(metapeptide_seq, min_aaqual,
+                                             len(nt_seq_for_full_aaseq), [metapeptide_nt_seq], 1))
 
-    # return the proteinlets and accounting information for this read
-    return result_proteinlets, (n_discarded_tooshort, n_discarded_minqualscore,
+    # return the metapeptides and accounting information for this read
+    return result_metapeptides, (n_discarded_tooshort, n_discarded_minqualscore,
                                 n_discarded_stopcodon, n_discarded_longestpep_tooshort,
                                 n_discarded_toofew_trypticsites)
 
 
-def read_proteinlets(proteinlet_file):
+def read_metapeptides(metapeptide_file):
     """
-    Read proteinlets from file.
-    :param proteinlet_database_file:
+    Read metapeptides from file.
+    :param metapeptide_database_file:
     :param countsfile:
     :return:
     """
-    metadata_reader = csv.DictReader(proteinlet_file, delimiter='\t')
+    metadata_reader = csv.DictReader(metapeptide_file, delimiter='\t')
     for row in metadata_reader:
-        proteinlet_seq = row['sequence']
+        metapeptide_seq = row['sequence']
         min_qualscore = int(row['min_qualscore'])
         partial_orf_length = int(row['partial_orf_length'])
         nt_seqs = row['nt_sequence'].split(',')
         n_reads = int(row['n_reads'])
 
-        proteinlet = Proteinlet(proteinlet_seq, min_qualscore,
+        metapeptide = Proteinlet(metapeptide_seq, min_qualscore,
                                 partial_orf_length, nt_seqs, n_reads)
-        yield proteinlet
+        yield metapeptide
 
 
-def write_proteinlets(proteinlets, out_file):
+def write_metapeptides(metapeptides, out_file):
     """
-    Write proteinlets to a file
-    :param proteinlets: iterator over proteinlets
+    Write metapeptides to a file
+    :param metapeptides: iterator over metapeptides
     :param out_file: output file
-    :return: number of proteinlets written
+    :return: number of metapeptides written
     """
 
     out_file.write('\t'.join(PROTEINLETDB_COLUMNS) + "\n")
     n_written = 0
-    for proteinlet in proteinlets:
-        out_file.write(proteinlet.make_output_line() + '\n')
+    for metapeptide in metapeptides:
+        out_file.write(metapeptide.make_output_line() + '\n')
         n_written += 1
     return n_written
 
 
-def filter_proteinlets(proteinlet_generator, min_orf_len, min_aa_seq_len,
+def filter_metapeptides(metapeptide_generator, min_orf_len, min_aa_seq_len,
                        min_readcount, min_qualscore, min_longest_tryp_pep_len,
-                       max_proteinlets=None):
+                       max_metapeptides=None):
     """
-    Apply filtering criteria to proteinlets, yielding the ones that pass as they're
+    Apply filtering criteria to metapeptides, yielding the ones that pass as they're
     encountered.
-    :param proteinlet_generator:
+    :param metapeptide_generator:
     :param min_orf_len:
     :param min_aa_seq_len:
     :param min_readcount:
     :param min_qualscore:
     :param min_longest_tryp_pep_len:
-    :param max_proteinlets: maximum number of proteinlets to return (for testing)
+    :param max_metapeptides: maximum number of metapeptides to return (for testing)
     :return:
     """
     n_yielded = 0
-    for proteinlet in proteinlet_generator:
-        if not proteinlet.passes_filter(min_aa_seq_len,
+    for metapeptide in metapeptide_generator:
+        if not metapeptide.passes_filter(min_aa_seq_len,
                                         min_orf_len,
                                         min_readcount, min_qualscore,
                                         min_longest_tryp_pep_len):
             continue
-        yield proteinlet
+        yield metapeptide
         n_yielded += 1
-        if max_proteinlets is not None and n_yielded >= max_proteinlets:
-            logger.debug("filter_proteinlets stopping early, after %d." % n_yielded)
+        if max_metapeptides is not None and n_yielded >= max_metapeptides:
+            logger.debug("filter_metapeptides stopping early, after %d." % n_yielded)
             break
 
 
 class Proteinlet(object):
     """
-    A class representing a proteinlet, carrying around all the information we might need later. Does not
+    A class representing a metapeptide, carrying around all the information we might need later. Does not
     track individual read IDs
     """
     def __init__(self, sequence, min_qualscore, partial_orf_len, nt_seqs,
@@ -314,7 +314,7 @@ class Proteinlet(object):
 
     def calc_longest_tryppep_length(self):
         """
-        Calculate the length of the longest tryptic peptide in this proteinlet
+        Calculate the length of the longest tryptic peptide in this metapeptide
         :return:
         """
         tryp_peps = TRYPTIC_CLEAVAGE_REGEX.findall(self.sequence)
@@ -325,7 +325,7 @@ class Proteinlet(object):
                       min_reads, min_qualscore,
                       min_longest_peptide_length):
         """
-        Does this proteinlet pass a set of filtering criteria?
+        Does this metapeptide pass a set of filtering criteria?
         :param min_aa_seq_length:
         :param min_orf_length:
         :param min_reads:
@@ -347,7 +347,7 @@ class Proteinlet(object):
 
     def make_output_line(self):
         """
-        make a line for this proteinlet in a database file.
+        make a line for this metapeptide in a database file.
         does not include \n
         :return:
         """
